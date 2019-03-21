00b2368ab68b3caa31f99215a4c62e76517f8a50
Tyler Stark <theirondeveloper@gmail.com>
2019-03-21 00:00:12 -0500

    Remove dot references and update test example

--- canvimation.js
+++ canvimation.js
@@ -1,41 +1,62 @@
 
 class RenderObject {
-  constructor(ctx) {
+  constructor(ctx, width, height) {
     this.ctx = ctx;
+    this.x = Math.random() * width;
+    this.y = Math.random() * height;
+    this.height = height;
+    this.width = width;
   }
-  draw() {}
-  update() {}
+  draw() {
+    this.ctx.fillRect(this.x, this.y, 5, 5);
+  }
+  update(width, height) {
+    if (width === this.width && this.height === height) return;
+    this.height = height;
+    this.width = width;
+    this.x = Math.random() * width;
+    this.y = Math.random() * height;
+  }
 }
 
 class CanvasScene {
-  constructor(window, document, dotCount = 100, dotClass = RenderObject) {
+  constructor(window, document, canvasId = 'canvas',renderObjectCount = 100, renderObjectClass = RenderObject) {
     this.window = window;
     this.document = document;
 
-    this.canvas = this.document.getElementById('canvas');
+    this.canvas = this.document.getElementById(canvasId);
     this.ctx = this.canvas.getContext('2d');
 
     this.width = this.canvas.offsetWidth; // Width of the scene
     this.height = this.canvas.offsetHeight; // Height of the scene
 
-    this.dotCount = dotCount;
-    this.DotClass = dotClass;
+    this.renderObjectCount = renderObjectCount;
+    this.RenderObjectClass = renderObjectClass;
 
-    this.dots = [];
+    this.renderObjects = [];
 
     this.bindResize();
+    this.resize(); // Get appropriate canvas constraints
     this.init();
   }
   animate() {
     this.window.requestAnimationFrame(() => this.animate());
-    this.dots.forEach(dot => dot.draw(this.ctx));
+    this.update();
+    this.clear();
+    this.draw();
   }
   bindResize() {
     this.window.addEventListener('resize', () => this.resize());
   }
+  clear() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+  }
+  draw() {
+    this.renderObjects.forEach(dot =>  dot.draw());
+  }
   init() {
-    let i = this.dotCount;
-    while (i--) this.dots.push(new this.DotClass());
+    let i = this.renderObjectCount;
+    while (i--) this.renderObjects.push(new this.RenderObjectClass(this.ctx, this.width, this.height));
   }
   resize() {
     this.width = this.canvas.offsetWidth;
@@ -54,7 +75,7 @@ class CanvasScene {
     this.window.requestAnimationFrame(() => this.animate())
   }
   update() {
-    this.dots.forEach(dot => dot.update());
+    this.renderObjects.forEach((dot) => dot.update(this.width, this.height));
   }
 }
 
